06a5c4e45a9fc5de0438d5a5201477c44a687455
guidoasbun <guido@asbun.io>
2021-09-26 11:42:28 -0700

    DArray2.h, DArray2.cpp

---
+++ cmake-build-debug/CMakeFiles/DArray2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DArray2.dir/DArray.cpp.o"
+  "CMakeFiles/DArray2.dir/main.cpp.o"
+  "DArray2"
+  "DArray2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DArray2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
